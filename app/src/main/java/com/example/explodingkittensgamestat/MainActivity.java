c961d1c2affd0489840a5bf42832e0c49899a106
nastase24 <89493347+nastase24@users.noreply.github.com>
2021-10-03 19:59:25 -0700

    GUI, initalized the cards

--- app/src/main/java/com/example/explodingkittensgamestat/MainActivity.java
+++ app/src/main/java/com/example/explodingkittensgamestat/MainActivity.java
@@ -3,10 +3,11 @@ package com.example.explodingkittensgamestat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     //these are for every player, so only one instance should be created
 
@@ -16,6 +17,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+    }
+
+    @Override
+    public void onClick(View view) {
 
     }
 }
